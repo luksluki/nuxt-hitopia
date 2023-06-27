a3b2cef52a288ca77399b8baee1ab9f89485a23f
luksluki <lukirahayuagung@gmail.com>
2023-06-27 10:51:45 +0700

    update nuxt config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,6 +5,7 @@ export default defineNuxtConfig({
     ],
     css: ['vuetify/lib/styles/main.sass', '@mdi/font/css/materialdesignicons.min.css',],
     build: {
+      dir: 'dist',
       transpile: ['vuetify'],
     },
     vite: {
